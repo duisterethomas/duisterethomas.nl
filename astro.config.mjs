2a921ff224b508ccfb7125dd3068cb23bcc4a29a
duisterethomas <duisterethomas@gmail.com>
2025-07-16 23:31:18 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://duisterethomas.nl",
+
     vite: {
         plugins: [tailwindcss()],
     },
+
+    integrations: [sitemap()],
 });
